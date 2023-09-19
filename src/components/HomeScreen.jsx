7db57103b559f5e1243df4f62ad341248fc5d120
Zbigniew Szalkowski <zbigniew.szalkowski@gmail.com>
2023-09-19 20:47:29 +0200

    feat: create welcome screen

---
+++ src/components/HomeScreen.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from './HomeScreen.module.css';
+import Line from '../../assets/Line 15.png';
+import Sword from '../../assets/image 5.png';
+import { ChooseButton } from './ChooseButton';
+
+export const HomeScreen = () => {
+   return (
+      <div className="container">
+         <div className={styles.title}>
+            <h1>Ultimate Pixel</h1>
+            <h1 style={{ color: '#DA8B14' }}>Championship</h1>
+         </div>
+         <div className={styles.line}>
+            <img src={Line} width={256} height={4} />
+         </div>
+         <div className={styles.swords}>
+            <img src={Sword} width={60} height={60} />
+         </div>
+         <div className={styles.description}>
+            <p>Fill out the form to sign up for upcoming tournee.</p>
+         </div>
+         <ChooseButton />
+      </div>
+   );
+};
