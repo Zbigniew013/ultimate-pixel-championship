d6bb19ef81e8bf1e4e40fd05dd501e42b1270cb7
Zbigniew Szalkowski <zbigniew.szalkowski@gmail.com>
2023-09-20 17:32:11 +0200

    feat: create step UI

---
+++ src/components/wizard-screen-components/FighterName.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './FighterName.module.css';
+
+export const FighterName = () => {
+   return (
+      <div className={styles.fighter_name}>
+         <h3>Annoyed Karen</h3>
+      </div>
+   );
+};
